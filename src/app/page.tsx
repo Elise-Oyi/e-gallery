8083d75e45a88cba55491c2a07ba50b29814f888
Elise-oyi <maryeliseangbo@gmail.com>
2024-10-17 21:41:41 +0000

    used auth for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { headers } from "next/headers";
 import Link from "next/link";
 import { db } from "~/server/db";
@@ -5,31 +6,33 @@ import { db } from "~/server/db";
 //-- to make the page dynamic and for the page to immediately recognise db changes
 export const dynamic = "force-dynamic"
 
-const mockUrls = [
-    "https://images.unsplash.com/photo-1502301197179-65228ab57f78?w=1400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8dmFjYXRpb258ZW58MHx8MHx8fDA%3D",
-    "https://plus.unsplash.com/premium_photo-1680497811614-4f93025d7e57?w=1400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8dmFjYXRpb258ZW58MHx8MHx8fDA%3D",
-    "https://images.unsplash.com/photo-1700220858049-dafb63e12bbf?w=1400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZnJlaWJ1cmclMjBpbSUyMGJyZWlzZ2F1fGVufDB8fDB8fHww",
-    
-  ]
 
-export default async function HomePage() {
+async function Images (){
 
   const images = await db.query.images.findMany({
     orderBy: (model, {desc}) => desc(model.id)
   })
+
+  return(
+    images.map((image, index)=>(
+      <div key={image.id + "-" + index} className="w-48 p-4">
+        <img src={image.url} />
+      </div>
+    ))
+  )
+}
+
+
+export default async function HomePage() {
   
-  const mockImages = mockUrls.map((url, index) => ({
-    id: index + 1,
-    url
-  }))
   return (
     <main className="flex flex-wrap gap-4">
-     
-      {images.map((image, index)=>(
-        <div key={image.id + "-" + index} className="w-48 p-4">
-          <img src={image.url} />
-        </div>
-      ))}
+     <SignedOut>
+      <div className="h-full w-full text2xl text-center">Please sign in above</div>
+     </SignedOut>
+     <SignedIn>
+       <Images/>
+     </SignedIn>
     </main>
   );
 }
