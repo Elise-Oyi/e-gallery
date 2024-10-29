123907667f1c3572eed3b43a1a1f319dcc694a64
Elise-oyi <maryeliseangbo@gmail.com>
2024-10-29 15:58:42 +0000

    added sentry and parallel route

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
- import "~/styles/globals.css";
- import "@uploadthing/react/styles.css";
+import "~/styles/globals.css";
+import "@uploadthing/react/styles.css";
 
- import {
-  ClerkProvider,
-} from '@clerk/nextjs'
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
 
 import { type Metadata } from "next";
 import { TopNav } from "./_components/topnav";
@@ -11,10 +10,10 @@ import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { extractRouterConfig } from "uploadthing/server";
 import { ourFileRouter } from "./api/uploadthing/core";
 
-
-// const inter = Inter({
-//   variable: "--font-sans"
-// })
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans"
+})
 
 export const metadata: Metadata = {
   title: "E Gallery",
@@ -22,15 +21,17 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-
-
 export default function RootLayout({
   children,
-}: Readonly<{ children: React.ReactNode }>) {
+  modal,
+}: { 
+    children: React.ReactNode;
+    modal: React.ReactNode 
+  }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-    <NextSSRPlugin
+      <html lang="en">
+        <NextSSRPlugin
           /**
            * The `extractRouterConfig` will extract **only** the route configs
            * from the router to prevent additional information from being
@@ -39,12 +40,13 @@ export default function RootLayout({
            */
           routerConfig={extractRouterConfig(ourFileRouter)}
         />
-      <body className={`font-sans  flex flex-col gap-4`}>
-      <TopNav/>
-
-        {children}
+        <body className={`flex flex-col gap-4 ${inter.variable}`}>
+          <TopNav />
+          {children}
+          {modal}
+          <div id="modal-root" />
         </body>
-    </html>
+      </html>
     </ClerkProvider>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Image from "next/image";
+import Link from "next/link";
 import { getMyImages } from "~/server/queries";
 
 //-- to make the page dynamic and for the page to immediately recognise db changes
@@ -9,20 +10,19 @@ async function Images() {
   const images = await getMyImages();
 
   return (
-    <div className=" flex flex-wrap justify-center gap-4">
+    <div className="flex w-full flex-wrap justify-center gap-4 p-3">
       {images.map((image) => (
-        <div
-          key={image.id}
-          className="flex h-48 w-48 flex-col"
-        >
-          <Image
-            src={image.url}
-            alt={image.name}
-            style={{ objectFit: "contain" }}
-            width={192}
-            height={192}
-          />
-          <div className="">{image.name}</div>
+        <div key={image.id} className="flex h-48 w-48 flex-col p-2">
+          <Link href={`/img/${image.id}`}>
+            <Image
+              src={image.url}
+              alt={image.name}
+              style={{ objectFit: "contain" }}
+              width={192}
+              height={192}
+            />
+          </Link>
+          <div className="text-wrap p-2">{image.name}</div>
         </div>
       ))}
     </div>
